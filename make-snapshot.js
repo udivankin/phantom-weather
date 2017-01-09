406efee54364629938dbe8e0f162e5e3edcb837c
Alexey Udivankin <alexey@udivankin.ru>
2017-01-10 00:15:53 +0200

    fix make snapshot script

--- make-snapshot.js
+++ make-snapshot.js
@@ -2,7 +2,10 @@ var URL = 'http://zengraph.ru/weather-display/'
 var page = require('webpage').create();
 page.viewportSize = { width: 600, height: 800 };
 page.clipRect = { top: 0, left: 0, width: 600, height: 800 };
-page.open(URL, function() {
-  page.render('snapshot.png');
-  phantom.exit();
-});
+
+page.onLoadFinished = function(status) {
+    page.render('snapshot.png');
+    phantom.exit();
+}
+
+page.open(URL);
